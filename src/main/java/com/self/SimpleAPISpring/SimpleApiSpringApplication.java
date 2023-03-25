421a036208cf9612b179075c54115d32e3d64f34
hamed.refaat <hamed.elhassanien@talabat.com>
2023-03-25 17:24:20 +0200

    inital commit

---
+++ src/main/java/com/self/SimpleAPISpring/SimpleApiSpringApplication.java
@@ -0,0 +1,13 @@
+package com.self.SimpleAPISpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleApiSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleApiSpringApplication.class, args);
+	}
+
+}
